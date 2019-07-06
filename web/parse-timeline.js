19496a2831f93850236d5fafa7b24229cb6207ff
Dimitrije Jankov <dimitrijejankov@gmail.com>
2019-07-06 15:30:00 -0500

    Made the parser work fast

--- web/parse-timeline.js
+++ web/parse-timeline.js
@@ -1,6 +1,5 @@
 function loadMe()
 {
-
     // conversion method
     function bin2String (array, offset, size) {
         var result = "";
@@ -8,6 +7,114 @@ function loadMe()
             result += String.fromCharCode(parseInt(array[offset + i], 2));
         }
         return result;
+    }
+
+    function parseTimeline(byteArray) {
+
+       // byte array
+       var byteView = new DataView(byteArray)
+
+       // the offset
+       var byteOffset = 0
+
+       // check the magic number
+       var magicNumber = byteView.getBigUint64(byteOffset, true);
+       if(magicNumber != 10202026){
+           alert("This is not a timeline file");
+       }
+       
+       // get the number of pages
+       var numberOfPages = byteView.getBigUint64(byteOffset + 8, true);
+
+       // get the page size
+       var pageSize = byteView.getBigUint64(byteOffset + 16, true);
+
+       // skip the first three numbers
+       byteOffset += 3 * 8
+
+       var timeline = { "pageSize" : pageSize, "numberOfPages" : numberOfPages, "snapshots" : {} };
+
+       while(byteView.byteLength != byteOffset) {
+           
+           // the snapshot object we are going to fill up
+           var snapshot = { "activePages" : [] , "freePages" : [] };
+
+           // get the tick of the snapshot
+           var tick = byteView.getBigUint64(byteOffset, true);
+
+           // get the number of pages
+           var activePages = byteView.getBigUint64(byteOffset + 8, true);
+
+           // remove the last two ints
+           byteOffset += 2 * 8
+
+           // preallocate the pages
+           snapshot["activePages"] = new Array(Number(activePages));
+
+           // get all the pages
+           var i;
+           for (i = 0; i < activePages; i++) {
+
+               // read in the size of the database name
+               var dbNameSize = byteView.getBigUint64(byteOffset, true);
+               byteOffset += 8;
+               
+               // read in a string (this is actually very painful to see so inefficient)
+               var dbName = bin2String(byteView, byteOffset, dbNameSize);
+               byteOffset += Number(dbNameSize);
+
+               // read in the size of the database name
+               var setNameSize = byteView.getBigUint64(byteOffset, true);
+               byteOffset += Number(8);
+               
+               // read in a string (this is actually very painful to see so inefficient)
+               var setName = bin2String(byteView, byteOffset, setNameSize);
+               byteOffset += Number(setNameSize);
+
+               // get the page number
+               var pageNumber = byteView.getBigUint64(byteOffset, true);
+               
+               // get the offset 
+               var pageOffset = byteView.getBigUint64(byteOffset + 8, true);
+
+               // get the page size
+               var pageSize = byteView.getBigUint64(byteOffset + 16, true);
+
+               // skip to the next
+               byteOffset += 24
+
+               snapshot["activePages"][i] = {"dbName" : dbName, "setName" : setName, "pageNumber" : pageNumber, "pageOffset" : pageOffset, "pageSize" : pageSize };
+           }
+
+           // get the number of pages
+           var emptyPages = byteView.getBigUint64(byteOffset, true);
+           byteOffset += 8;
+
+           // preallocate the pages
+           snapshot["freePages"] = new Array(Number(emptyPages));
+
+           // read in the empty pages
+           for (i = 0; i < emptyPages; i++) {
+               
+               // get the offset of a page
+               var offset = byteView.getBigUint64(byteOffset + i * 16, true);
+
+               // get the page size
+               var pageSize = byteView.getBigUint64(byteOffset + i * 16 + 8, true);
+
+               // set the free page
+               snapshot["freePages"][i] = { "offset" : offset, "pageSize" : pageSize }
+           }
+
+           // skip the stuff we just read
+           byteOffset += Number(emptyPages) * Number(16)
+           
+           // add the new snapshot
+           timeline["snapshots"][tick] = snapshot;
+       }
+
+       // return the timeline
+       return timeline;
     }
 
     var fileInput = document.getElementById('browseOpen');
@@ -18,106 +125,11 @@ function loadMe()
         // grab the byte array
         var byteArray = e.target.result
 
-        // to read the uint64 types
-        var uint64View = new BigUint64Array(byteArray);
+        // parse the timeline
+        var timeline = parseTimeline(byteArray);
 
-        // byte array
-        var byteView = new Int8Array(byteArray)
-
-        // check the magic number
-        var magicNumber = uint64View[0];
-        if(magicNumber != 10202026){
-            alert("This is not a timeline file");
-        }
-        
-        // get the number of pages
-        var numberOfPages = uint64View[1];
-
-        // get the page size
-        var pageSize = uint64View[2];
-
-        // skip the first three numbers
-        uint64View = uint64View.slice(3);
-
-        var timeline = { "pageSize" : pageSize, "numberOfPages" : numberOfPages, "snapshots" : {} };
-
-        while(uint64View.byteLength != 0) {
-            
-            // the snapshot object we are going to fill up
-            var snapshot = { "activePages" : [] , "freePages" : [] };
-
-            // get the tick of the snapshot
-            var tick = uint64View[0];
-
-            // get the number of pages
-            var activePages = uint64View[1];
-
-            // remove the last two ints
-            uint64View = uint64View.slice(2);
-
-            // preallocate the pages
-            snapshot["activePages"] = new Array(Number(activePages));
-
-            // get all the pages
-            var i;
-            for (i = 0; i < activePages; i++) {
-
-                // read in the size of the database name
-                var dbNameSize = uint64View[0];
-                uint64View = uint64View.slice(1);
-                
-                // read in a string (this is actually very painful to see so inefficient)
-                var offset = byteArray.byteLength - uint64View.byteLength
-                var dbName = bin2String(byteView, byteArray.byteLength - uint64View.byteLength, dbNameSize);
-                uint64View = new BigUint64Array(byteArray.slice(offset + Number(dbNameSize)));
-
-                // read in the size of the database name
-                var setNameSize = uint64View[0];
-                uint64View = uint64View.slice(1);
-                
-                // read in a string (this is actually very painful to see so inefficient)
-                var offset = byteArray.byteLength - uint64View.byteLength
-                var setName = bin2String(byteView, byteArray.byteLength - uint64View.byteLength, setNameSize);
-                uint64View = new BigUint64Array(byteArray.slice(offset + Number(dbNameSize)));
-
-                // get the page number
-                var pageNumber = uint64View[0];
-                
-                // get the offset 
-                var pageOffset = uint64View[1];
-
-                // get the page size
-                var pageSize = uint64View[2];
-
-                // skip to the next
-                uint64View = uint64View.slice(3);
-
-                snapshot["activePages"][i] = {"dbName" : dbName, "setName" : setName, "pageNumber" : pageNumber, "pageOffset" : pageOffset, "pageSize" : pageSize };
-            }
-
-            // get the number of pages
-            var emptyPages = uint64View[0];
-            uint64View = uint64View.slice(1);
-
-            // preallocate the pages
-            snapshot["freePages"] = new Array(Number(emptyPages));
-
-            // read in the empty pages
-            for (i = 0; i < emptyPages; i++) {
-                
-                // get the offset of a page
-                var offset = uint64View[i * 2];
-
-                // get the page size
-                var pageSize = uint64View[i * 2 + 1];
-            }
-
-            // skip the stuff we just read
-            uint64View = uint64View.slice(Number(emptyPages) * 2);
-            
-            // add the new snapshot
-            timeline["snapshots"][tick] = snapshot;
-        }
+        // finished parsing
+        log("Finished parsing");
     }
     
     fileInput.onchange = function(e) {
